47d64cdad77e9ee053b6bcb9fc3a5501c19cbccc
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-05-29 16:38:43 -0400

    clean up

--- 05-Third-Party-APIs/README.md
+++ 05-Third-Party-APIs/README.md
@@ -1,5 +1,7 @@
 # 05 Third-Party APIs: Work Day Scheduler
 
+Deployed @ https://while-alive.github.io/Challenges/05-Third-Party-APIs/
+
 Create a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 The starter code uses the [Moment.js](https://momentjs.com/) library to work with date and time, but feel free to use a different JavaScript solution to handle this functionality since Moment.js is considered a "legacy" project. Learn more about these other solutions in the [Moment.js project status page.](https://momentjs.com/docs/#/-project-status/)  
--- 05-Third-Party-APIs/assets/js/script.js
+++ 05-Third-Party-APIs/assets/js/script.js
@@ -1,41 +1,39 @@
 // TO DO LATER .. make time blocks meters that move with the passage of time
 // how graphically intensive is this???
 
-// timeblock create & update - not super efficient but it works
+// timeblock create & update
 function update_timeblock_func(user_select_start, user_select_end) {
 
     // target container element and create time-block elements
     var container_El = $(".container");
     var time_block_El = $("<div></div>")
-        .addClass("row time-block")
+        .addClass("row time-block");
     var hour_El = $("<div></div>")
-        .addClass("col-1 hour")
+        .addClass("col-1 hour");
     var content_El = $("<textarea></textarea>")
         .attr("id", "textarea")
-        .addClass("col-10")
+        .addClass("col-10");
     var saveBtn_El = $("<button></button>")
         .addClass("col-1 saveBtn")
-        .html("<i class='fas fa-plus'></i>")
+        .html("<i class='fas fa-plus'></i>");
 
-    // getting the hour(24H) the function is called
-    var this_moment = parseInt(moment().format('H')) //safety first
+    // getting the current hour(24H)
+    var this_moment = parseInt(moment().format('H')); //safety first
 
     var classy = ""; // challenge pre-determined css classes
 
+    // for x (range of hours)
     for (var i = user_select_start; i < user_select_end; i++) {
-        // determines colors of textarae
+        // determine colors of textarea
         if (i < this_moment) {
             classy = "past";
-
         } else if (i == this_moment) {
             classy = "present";
-
         } else {
             classy = "future";
-        }
+        };
+
         // create time-blocks : hour-text-save
-        // then clone and append them to the container
-        // for x (range of hours selected)
         time_block_El
             .html(//add hour, textarea, and save button
                 hour_El.html(
@@ -48,34 +46,32 @@ function update_timeblock_func(user_select_start, user_select_end) {
                     .addClass(classy)
                     .text(tasks[i]),
                 saveBtn_El
-            )
+            );
+        // clone and append them to the container
         container_El.append(
             time_block_El.clone()
                 //set time-block ID to corresponding hour
                 .attr("id", (moment().set('hour', i).format('H')))
-        )
-    }
-}
+        );
+    };
+};
 
+// the new-user / on-refresh function
 function update_tasks_func(user_select_start, user_select_end) {
     // pull tasks from localstorage if it is there
     tasks = JSON.parse(localStorage.getItem("tasks"));
-
     var this_textarea;
-
     if (!tasks) { // if still null . initalize it
-        tasks = {}
+        tasks = {};
         for (var i = user_select_start; i < user_select_end; i++) {
-            tasks[i] = ""
-        }
-    }
-
+            tasks[i] = "";
+        };
+    };
     // update on screen tasks 
     for (var i = user_select_start; i < user_select_end; i++) {
-        this_textarea = $(String("#" + i)).find("textarea").val()
-        console.log(this_textarea)
+        this_textarea = $(String("#" + i)).find("textarea").val();
     }
-}
+};
 
 // global time
 var time = 0;
@@ -88,45 +84,38 @@ var update_time = setInterval(function () {
         .html(
             time.toString().substr(0, 13)
             + "\n\n" + time.toString().substr(15).toUpperCase()
-        )
+        );
     // TO DO LATER // Make a clock widget for this .. with rotating circle? on top of SVG? // maybe canvas?
 
     // update the time-block colors every hour
-    // does not have adverse effects when called
-    // but I am keeping an eye on it
     if (moment().format("mm:ss") == "00:00"){
-        console.log("updating timeblocks")
+        console.log("updating timeblocks");
         update_timeblock_func();
     }
 }, 1000);
 
-// TO-DO : user should be able to choose 
-// when to start and end their day
+// TO-DO : Would like to allow minor adjustments 
+// from user of start and end times
 var user_select_start = 9; //9am
 var user_select_end = 17 + 1; //5pm : i believe i need to add 1 to user input
 
 // global task object / localstorage key
 var tasks = {};
 
-update_tasks_func(user_select_start, user_select_end)
-update_timeblock_func(user_select_start, user_select_end)
+update_tasks_func(user_select_start, user_select_end);
+update_timeblock_func(user_select_start, user_select_end);
 
 // saveBtn on click
 $(".saveBtn").on("click", function () {
-
     // previous element in timeblock "textarea"
     var this_previous = $(($(this))[0].previousElementSibling);
-
+    // its value 
     var this_previous_val = this_previous.val().trim();
-
+    // block id
     var this_block_id = this_previous[0].parentElement.id;
 
-    // if (content) {add to task object & push to localstorage}
-    if (this_previous_val) {
-        console.log("Text: " + this_previous_val); // textarea value
-        console.log("Block ID: " + this_block_id);
-    }
+    // add only current block to the array
     tasks[this_block_id] = this_previous_val;
-
+    // push to localstorage
     localStorage.setItem("tasks", JSON.stringify(tasks));
 });
