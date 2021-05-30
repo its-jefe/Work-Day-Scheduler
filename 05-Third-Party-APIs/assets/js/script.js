1f1ca91841ad4149d6e563ebadee1b3c6f28ed97
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-05-29 20:35:17 -0400

    added a new function to take care of colorization

--- 05-Third-Party-APIs/assets/js/script.js
+++ 05-Third-Party-APIs/assets/js/script.js
@@ -1,8 +1,8 @@
 // TO DO LATER .. make time blocks meters that move with the passage of time
 // how graphically intensive is this???
 
-// timeblock create & update
-function update_timeblock_func(user_select_start, user_select_end) {
+// timeblock create
+function create_timeblock_func(user_select_start, user_select_end) {
 
     // target container element and create time-block elements
     var container_El = $(".container");
@@ -11,15 +11,56 @@ function update_timeblock_func(user_select_start, user_select_end) {
     var hour_El = $("<div></div>")
         .addClass("col-1 hour");
     var content_El = $("<textarea></textarea>")
+        .addClass("col-10")
         .attr("id", "textarea")
-        .addClass("col-10");
     var saveBtn_El = $("<button></button>")
         .addClass("col-1 saveBtn")
         .html("<i class='fas fa-plus'></i>");
 
-    // getting the current hour(24H)
-    var this_moment = parseInt(moment().format('H')); //safety first
+    for (var i = user_select_start; i < user_select_end; i++) {
 
+        // create time-blocks : hour-text-save
+        time_block_El
+            .html(//add hour, textarea, and save button
+                hour_El.html(
+                    "<span>" +
+                    (moment().set('hour', i).format('h a')).toUpperCase()
+                    + "</span>")
+                    .add(content_El)
+                    .add(saveBtn_El),
+                content_El
+                    .text(schedule[i]),
+                saveBtn_El
+            );
+        // clone and append them to the container
+        container_El.append(
+            time_block_El.clone()
+                // set time-block ID to corresponding hour NOTE: can only manage 0-23
+                .attr("id", (moment().set('hour', i).format('H')))
+        );
+    };
+};
+
+// the new-user / on-refresh function
+function update_tasks_func(user_select_start, user_select_end) {
+    // pull tasks from localstorage if it is there
+    schedule = JSON.parse(localStorage.getItem("schedule"));
+    var this_textarea;
+    if (!schedule) { // if still null . initalize it
+        schedule = {};
+        for (var i = user_select_start; i < user_select_end; i++) {
+            schedule[i] = "";
+        };
+    };
+    // update on screen tasks 
+    for (var i = user_select_start; i < user_select_end; i++) {
+        this_textarea = $(String("#" + i)).find("textarea").val();
+    }
+};
+
+function update_timeblock_func() {
+    // getting the current hour(24H)
+    var this_moment = parseInt(moment().format('H'));
     var classy = ""; // challenge pre-determined css classes
 
     // for x (range of hours)
@@ -33,46 +74,12 @@ function update_timeblock_func(user_select_start, user_select_end) {
             classy = "future";
         };
 
-        // create time-blocks : hour-text-save
-        time_block_El
-            .html(//add hour, textarea, and save button
-                hour_El.html(
-                    "<span>" +
-                    (moment().set('hour', i).format('h a')).toUpperCase()
-                    + "</span>")
-                    .add(content_El)
-                    .add(saveBtn_El),
-                content_El
-                    .addClass(classy)
-                    .text(tasks[i]),
-                saveBtn_El
-            );
-        // clone and append them to the container
-        container_El.append(
-            time_block_El.clone()
-                //set time-block ID to corresponding hour
-                .attr("id", (moment().set('hour', i).format('H')))
-        );
+        $(String("#" + i)).find("textarea")
+            .removeClass()
+            .addClass(classy)
+            .addClass("col-10")
     };
 };
-
-// the new-user / on-refresh function
-function update_tasks_func(user_select_start, user_select_end) {
-    // pull tasks from localstorage if it is there
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-    var this_textarea;
-    if (!tasks) { // if still null . initalize it
-        tasks = {};
-        for (var i = user_select_start; i < user_select_end; i++) {
-            tasks[i] = "";
-        };
-    };
-    // update on screen tasks 
-    for (var i = user_select_start; i < user_select_end; i++) {
-        this_textarea = $(String("#" + i)).find("textarea").val();
-    }
-};
-
 // global time
 var time = 0;
 // endless interval updates every second
@@ -88,21 +95,22 @@ var update_time = setInterval(function () {
     // TO DO LATER // Make a clock widget for this .. with rotating circle? on top of SVG? // maybe canvas?
 
     // update the time-block colors every hour
-    if (moment().format("mm:ss") == "00:00"){
+    if (moment().format("mm:ss") == "00:00") {
         console.log("updating timeblocks");
-        update_timeblock_func();
+        update_timeblock_func(user_select_start, user_select_end)
     }
 }, 1000);
 
 // TO-DO : Would like to allow minor adjustments 
 // from user of start and end times
 var user_select_start = 9; //9am
-var user_select_end = 17 + 1; //5pm : i believe i need to add 1 to user input
+var user_select_end = 21 + 1; //5pm : i believe i need to add 1 to user input
 
 // global task object / localstorage key
-var tasks = {};
+var schedule = {};
 
 update_tasks_func(user_select_start, user_select_end);
+create_timeblock_func(user_select_start, user_select_end);
 update_timeblock_func(user_select_start, user_select_end);
 
 // saveBtn on click
@@ -115,7 +123,7 @@ $(".saveBtn").on("click", function () {
     var this_block_id = this_previous[0].parentElement.id;
 
     // add only current block to the array
-    tasks[this_block_id] = this_previous_val;
+    schedule[this_block_id] = this_previous_val;
     // push to localstorage
-    localStorage.setItem("tasks", JSON.stringify(tasks));
+    localStorage.setItem("schedule", JSON.stringify(schedule));
 });
